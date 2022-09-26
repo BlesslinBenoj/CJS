bc30f4d719ceff485cec0f4ab5d924f000d8d517
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-09-27 00:13:52 +0530

    basic   JS

---
+++ dynamic typing.js
@@ -0,0 +1,9 @@
+let firstname = "Blesslin";
+
+let age = 21;
+
+let isyoung = true
+
+let lastname = undefined;
+
+let trophy = null
---
+++ objects.js
@@ -0,0 +1,22 @@
+
+let person = {
+
+  name : "Blesslin benoj",
+
+  age : 21,
+
+  gender : "male",
+
+  address : "Kanyakumari, Tamil nadu , India",
+
+  sibling :
+  {
+
+    brother : "risk",
+
+    sister : "logic"
+  }
+
+}
+
+console.log(person.sibling.brother)
