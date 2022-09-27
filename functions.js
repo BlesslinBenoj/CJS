329ad738d97bbbbb6f31c9583504e8ff244f8fab
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-09-28 00:02:50 +0530

    functions in JS

---
+++ functions.js
@@ -0,0 +1,20 @@
+
+
+function greetuser(firstname,lastname){
+
+  let msg = " hello " + firstname + " " + lastname + " I'm learning javascript " ;
+
+  console.log(msg)
+
+}
+
+greetuser("Blesslin","benoj")
+
+
+
+function add(number1,number2){
+
+  console.log(number1 + number2)
+}
+
+add(65,89)
