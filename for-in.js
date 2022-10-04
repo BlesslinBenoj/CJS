0a4bbd81420f1f22db04c30dc58a2a23fb7a1be9
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-10-04 23:45:52 +0530

    For in loops

---
+++ for-in.js
@@ -0,0 +1,26 @@
+//1
+
+const person = {
+
+  name : "Blesslin benoj",
+  age : 21,
+  sex : "male"
+
+};
+
+for(let key in person){
+
+  console.log(key + " :", person[key]);
+
+}
+
+
+//2
+
+let colors = ["red" , "blue" , "green"];
+
+for(let key in colors){
+
+  console.log(colors[key]);
+  
+}
