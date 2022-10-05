2fb6018dd23d9592e0059f2cd51ed55f14bc1a91
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-10-06 04:24:36 +0530

    OBJECT ORIENTED PROGRAMMING

---
+++ OOP.js
@@ -0,0 +1,22 @@
+//OBJECT ORIENTED PROGRAMMING
+
+
+let person = {
+
+  name: "Blesslin benoj",
+  age: 21,
+  interest: ["creating,learning new things."],
+  address: {
+    city: "chennai",
+    state: "tamil nadu",
+
+  },
+  greeting: function () {
+    //let msg = "my name is "+ name +" i love "+ interest;
+    let msg = `my name is ${this.name},i love ${this.interest}`;
+
+    console.log(msg);
+  },
+};
+
+console.log(person.greeting());
