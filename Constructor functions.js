9e5a865b5bb728777afd290ee6d8da88eb8a8ac4
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-10-08 00:13:37 +0530

    Constructor Functions

---
+++ Constructor functions.js
@@ -0,0 +1,17 @@
+
+
+// pascal case = Person
+
+function Person(name){
+
+  this.name = name,
+  this.greeting = function(){
+
+    console.log(`My name is ${this.name}`);
+  }
+
+}
+
+let person = new Person("Blesslin");
+
+person.greeting();
