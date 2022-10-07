1d7d361c334078a4eaa98ef27a3b5fcc895ecc1f
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-10-08 00:13:12 +0530

    Factory Functions

---
+++ js factory functions.js
@@ -0,0 +1,23 @@
+
+// camelcase  = createPerson
+
+
+function createPerson(name){
+
+  return {
+
+    name,
+    greeting() {
+
+      let msg = `My name is ${this.name}`;
+      console.log(msg);
+
+    }
+  };
+}
+
+let blesslin = createPerson("Blesslin");
+let jerry = createPerson("Jerry")
+
+blesslin.greeting();
+jerry.greeting();
