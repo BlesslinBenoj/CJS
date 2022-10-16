3c9d1548698a710af6c475a1def9b0870235e7bf
BlesslinBenoj <85760673+BlesslinBenoj@users.noreply.github.com>
2022-10-16 23:55:35 +0530

    Javascript Constructor Property

---
+++ constructor property.js
@@ -0,0 +1,11 @@
+ // let name = "Blesslin";  //new String ("Blesslin")
+// let age = 21;  // number literals = new Number ()
+// let isAlive = true; // boolean literals = new Boolean ()
+
+// console.log(name,age,isAlive);
+
+let name = new String ("Blesslin");
+let age =  new Number (21);
+let isAlive = new Boolean (true);
+
+console.log(name,age,isAlive);
